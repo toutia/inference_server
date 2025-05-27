ba26906d85582326814563d871f10b3702629792
Ayoub TOUTI <ayoub.touti@icloud.com>
2025-05-27 12:06:59 +0200

    adjustment after testing

--- xttsv2/setup_xtts.sh
+++ xttsv2/setup_xtts.sh
@@ -11,9 +11,10 @@ echo "Downloading XTTSv2 model files from Hugging Face..."
 wget https://huggingface.co/coqui/XTTS-v2/resolve/main/model.pth -O "$MODEL_DIR/model.pth"
 wget https://huggingface.co/coqui/XTTS-v2/resolve/main/config.json -O "$MODEL_DIR/config.json"
 wget https://huggingface.co/coqui/XTTS-v2/resolve/main/speakers_xtts.pth -O "$MODEL_DIR/speakers_xtts.pth"
+wget https://huggingface.co/coqui/XTTS-v2/resolve/main/vocab.json -O "$MODEL_DIR/vocab.json"
 
 
-
+sudo chown -R touti:touti ../model_repository
 
 cp -rf   xtts  ../model_repository/models
 
